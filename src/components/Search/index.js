059ef9078062e6ea3e195d834cc3d3ac50ff6d4a
Everton747 <everton_747@hotmail.com>
2020-04-20 02:47:04 -0300

    added api connection

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,39 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 import './styles.scss';
 
 export default function Search() {
+
+  const [zeroKm, updateZeroKm] = useState(false);
+  const [used, updateUsed] = useState(false);
+
+  const [city, updateCity] = useState('São Paulo');
+
+  const [make, updateMake] = useState([]);
+  const [model, updateModel] = useState([]);
+  const [version, updateVersion] = useState([]);
+
+  const [year, updateYear] = useState('');
+  const [priceRate, updatePriceRate] = useState('');
+
+  useEffect(() => {
+    const response = api.get('/make');
+    response.then(res => { updateMake(res.data) });
+  }, []);
+
+  function handleUpdateModels(e) {
+    const response = api.get('/model/', { params: { 'MakeID': e.target.value } });
+    response.then(res => { updateModel(res.data) });
+  }
+
+  function handleUpdateVersions(e) {
+    const response = api.get('/version/', { params: { 'ModelID': e.target.value } });
+    response.then(res => { updateVersion(res.data) });
+  }
+
   return (
     <>
       <section className="inputs">
         <h2 className="active">Comprar Carros</h2>
         <h2>Comprar Motos</h2>
         <div className="new-used">
-          <div className="checkbox checked">✔ Novos</div>
-          <div className="checkbox">✔ Usados</div>
+          <div className={zeroKm ? 'checkbox checked' : 'checkbox'} onClick={e => updateZeroKm(!zeroKm)}>✔ Novos</div>
+          <div className={used ? 'checkbox checked' : 'checkbox'} onClick={e => updateUsed(!used)}>✔ Usados</div>
         </div>
         <div className="input-group">
           Onde:
-          <input type="text" className="city" value='São Paulo' />
+          <input type="text" className="city" value={city} onChange={e => updateCity(e.target.value)} />
           <div className="select-group">
             Raio:
             <select type="text" className="radius" placeholder='Raio'>
-              <option value="" selected>100km</option>
+              <option value="20">20km</option>
+              <option value="40">40km</option>
+              <option value="100">100km</option>
             </select>
           </div>
         </div>
         <div className="select-group">
           Marca:
-          <select type="text" className="vehicle-make">
-            <option value="">Todas</option>
-            <option value="">Chevrolet</option>
+          <select type="text" className="vehicle-make" onChange={handleUpdateModels}>
+            <option>Todas</option>
+            {make.map(make => (
+              <option key={make.ID} value={make.ID}>{make.Name}</option>
+            ))}
           </select>
         </div>
         <div className="select-group">
           Modelos:
-          <select type="text" className="vehicle-model">
-            <option value="">Todos</option>
-            <option value="">Fusca</option>
+          <select type="text" className="vehicle-model" onChange={handleUpdateVersions}>
+            <option>Todos</option>
+            {model.map(model => (
+              <option key={model.ID} value={model.ID}>{model.Name}</option>
+            ))}
           </select>
         </div>
         <select type="text" className="vehicle-year">
@@ -45,7 +81,10 @@ export default function Search() {
         <div className="select-group input-group">
           Versão:
           <select type="text" className="vehicle-version">
-            <option value="">Todas</option>
+            <option>Todas</option>
+            {version.map(version => (
+              <option key={version.ID} value={version.ID}>{version.Name}</option>
+            ))}
           </select>
         </div>
 
@@ -55,7 +94,6 @@ export default function Search() {
           <button>Ver Ofertas</button>
         </div>
       </section>
-
     </>
   );
 }
